48f1ff7984bfe0c6ded8eba1dbbb9202a316d94e
Leyrus <victor.leyrus@gmail.com>
2020-02-27 01:07:42 +0300

    AV-49

--- src/pages/chat/MainChat/index.jsx
+++ src/pages/chat/MainChat/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import SendIcon from '@material-ui/icons/Send';
 
 import { selectActiveChat, selectMessages } from '../../../store/chat/selectors';
 import { sendMessage, getMessages as getMessagesFromApi } from '../../../api';
@@ -30,6 +33,12 @@ export class MainChatComponent extends Component {
         }, 1000);
     }
 
+    componentDidUpdate() {
+        if(this.messageWrapper){
+            this.messageWrapper.scrollTop = 9999;
+        }
+    }
+
     componentWillUnmount() {
         clearInterval(this.intervalCheckMessage);
     }
@@ -41,23 +50,46 @@ export class MainChatComponent extends Component {
         this.setState({ messageText: '' });
     }
 
-    onChangeMessage = e => this.setState({ messageText: e.target.value })
+    onPressEnter = event => event.key === 'Enter' && this.onSendMessage();
+
+    onChangeMessage = event => this.setState({ messageText: event.target.value })
 
     render() {
+        const hasActiveChat = !_.isEmpty(this.props.activeChat);
+
         return (
             <div className={styles.wrapper}>
-                {!_.isEmpty(this.props.activeChat) ? (
-                    <div>
-                        <div>{`Active chat: ${this.props.activeChat.name}`}</div>
-                        {this.props.messages.map(message => (
-                            <div key={message.messageId}>
-                                {`${message.author.login}: ${message.content}`}
-                            </div>
-                        ))}
-                        <input type="text" onChange={this.onChangeMessage} value={this.state.messageText} />
-                        <button onClick={this.onSendMessage}>Send message</button>
-                    </div>
-                ) : <div>Choose a chat</div>}
+                <div className={styles.topBlock}>
+                    {hasActiveChat ? `Active chat: ${this.props.activeChat.name}` : 'Choose a chat'}
+                </div>
+                { hasActiveChat && (
+                    <>
+                        <div ref={(ref) => this.messageWrapper = ref} className={styles.messageWrapper}>
+                            {this.props.messages.map(message => (
+                                <div
+                                    key={message.messageId}
+                                    className={`${styles.message} 
+                                        ${this.props.userId === message.author.userId && styles.myMessage }`}
+                                >
+                                    {`${message.author.login}: ${message.content}`}
+                                </div>
+                            ))}
+                        </div>
+                        <div className={styles.form}>
+                            <TextField
+                                id="standard-basic"
+                                color="primary"
+                                label="Enter Your Message"
+                                onChange={this.onChangeMessage}
+                                onKeyUp={this.onPressEnter}
+                                value={this.state.messageText}
+                            />
+                            <Button variant="contained" color="primary" onClick={this.onSendMessage}>
+                                <SendIcon />
+                            </Button>
+                        </div>
+                    </>
+                )}
             </div>
         );
     }
