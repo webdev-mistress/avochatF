25521c2b2e0499a1acdaa845ba840453b8e9b8b1
Victor Plonin <victor.plonin@gmail.com>
2020-07-01 06:15:22 +0000

    Merged in feature/AV-58 (pull request #8)
    feature/AV-58: change api

---
+++ src/helpers/api.js
@@ -0,0 +1,22 @@
+const createHeader = (body) => ({
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(body),
+});
+
+const devMode = process.env.devMode === 'production' || true;
+const baseUrl = devMode ? 'http://localhost:4000' : 'http://80.87.201.216:4000';
+
+export const getResource = async (url, body) => {
+    const response = await fetch(`${baseUrl}${url}`, body && createHeader(body));
+
+    if (!response.ok) {
+        throw response.status;
+    }
+
+    return response.json();
+};
+
--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,53 +1,50 @@
-const createHeader = (body) => ({
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    },
-    method: 'POST',
-    body: JSON.stringify(body),
-});
+/* user */
+import { getResource } from '@/helpers/api';
 
-const devMode = process.env.devMode === 'production' || false;
-const baseUrl = devMode ? 'http://localhost:4170' : 'http://80.87.201.216:4000';
+const PREFIX_USER = '/user';
+const PREFIX_MESSAGES = '/messages';
+const PREFIX_CHAT = '/chat';
 
-const getResourse = async (url, body) => {
-    const response = await fetch(`${baseUrl}${url}`, body && createHeader(body));
-
-    if (!response.ok) {
-        throw response.status;
-    }
-
-    return response.json();
+export const getUser = function (user) {
+    return getResource(`${PREFIX_USER}/getUser`, user);
 };
 
-/* user */
-export const getUser = user => getResourse('/user/get', user);
-
-export const createUser = (name, login, password1, password2) =>
-    getResourse('/user/create', { name, login, password1, password2 });
+export const createUser = function (name, login, password1, password2) {
+    return getResource(`${PREFIX_USER}/createUser`, { name, login, password1, password2 });
+};
 
 /* messages */
-export const getMessages = (chatId) => getResourse('/messages/get', { chatId });
+export const getMessages = function (chatId) {
+    return getResource(`${PREFIX_MESSAGES}/getMessages`, { chatId });
+};
 
-export const sendMessage = (login, chatId, message) => getResourse('/messages/send', ({ login, chatId, message }));
+export const sendMessage = function (login, chatId, message) {
+    return getResource(`${PREFIX_MESSAGES}/sendMessage`, ({ login, chatId, message }));
+};
 
-export const deleteMessage = messageId => getResourse('/messages/delete',{ messageId });
+export const deleteMessage = function (messageId) {
+    return getResource(`${PREFIX_MESSAGES}/deleteMessage`, { messageId });
+};
 
-export const editMessage = (messageId, message) => getResourse('/messages/edit', { messageId, message });
+export const editMessage = function (messageId, message) {
+    return getResource(`${PREFIX_MESSAGES}/editMessage`, { messageId, message });
+};
 
 /* chat */
 
-export const createChat = (chatName, login) => getResourse('/chat/createChat', { chatName, login });
+export const createChat = function (chatName, login) {
+    return getResource(`${PREFIX_CHAT}/createChat`, { chatName, login });
+};
 
-export const deleteChat = (chatId) => getResourse('/chat/deleteChat', { chatId });
+export const deleteChat = function (chatId) {
+    return getResource(`${PREFIX_CHAT}/deleteChat`, { chatId });
+};
 
-export const addUserToChat = (login, chatId) => getResourse('/chat/addUser', { login, chatId });
+export const addUserToChat = function (login, chatId) {
+    return getResource(`${PREFIX_CHAT}/addUserToChat`, { login, chatId });
+};
 
-export const deleteUserFromChat = (login, chatId) => getResourse('/chat/deleteUser', { login, chatId });
+export const deleteUserFromChat = function (login, chatId) {
+    return getResource(`${PREFIX_CHAT}/deleteUserFromChat`, { login, chatId });
+};
 
-window.createChat = createChat;
-window.addUserToChat = addUserToChat;
-window.deleteUserFromChat = deleteUserFromChat;
-window.createUser = createUser;
-window.deleteChat = deleteChat;
-window.editMessage = editMessage;
